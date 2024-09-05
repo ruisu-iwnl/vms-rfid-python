5a1009ed323da5d2d76fa44c0ec781429ad54260
Luis Pelaez <119474028+ruisu666@users.noreply.github.com>
2024-09-06 02:59:56 +0800

    added dummy files

---
+++ app/routes/login/user_login.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from ..forms import UserLoginForm
+
+user_login_bp = Blueprint('user_login', __name__)
+
+@user_login_bp.route('/login', methods=['GET', 'POST'])
+def user_login():
+    form = UserLoginForm()
+    if form.validate_on_submit():
+        # Add logic to authenticate user
+        return redirect(url_for('main.index'))
+    return render_template('login/user_login.html', form=form)
---
+++ app/routes/register/user_register.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from ..forms import UserRegisterForm
+
+user_register_bp = Blueprint('user_register', __name__)
+
+@user_register_bp.route('/register', methods=['GET', 'POST'])
+def user_register():
+    form = UserRegisterForm()
+    if form.validate_on_submit():
+        # Add logic to register user
+        return redirect(url_for('user_login.user_login'))
+    return render_template('register/user_register.html', form=form)
