57a2ba8ff4dd6cb2db05b0faad520f7437d0a49c
Luis Pelaez <e.luis.pelaez@gmail.com>
2024-09-14 00:01:49 +0800

    added .env

--- app/models/database.py
+++ app/models/database.py
@@ -1,13 +1,17 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
 from mysql.connector import Error
+
+load_dotenv()
 
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="testdb"
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
         return connection
     except Error as err:
