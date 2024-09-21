96c865e0f10ec8e4589005f8f0ba59e54c76bdad
Luis Pelaez <e.luis.pelaez@gmail.com>
2024-09-22 01:44:59 +0800

    new database file

--- setup_files/updated_db(2).sql
+++ setup_files/updated_db(2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 21, 2024 at 03:37 PM
+-- Generation Time: Sep 21, 2024 at 07:10 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -61,6 +61,13 @@ CREATE TABLE `admin_activity_log` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `admin_activity_log`
+--
+
+INSERT INTO `admin_activity_log` (`activity_id`, `admin_id`, `activity_type`, `activity_timestamp`, `details`, `created_at`) VALUES
+(7, 3, 'Login successful', '2024-09-22 00:45:37', 'Login successful - User ID: 3', '2024-09-22 00:45:37');
+
 -- --------------------------------------------------------
 
 --
@@ -73,6 +80,19 @@ CREATE TABLE `rfid` (
   `vehicle_id` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rfid`
+--
+
+INSERT INTO `rfid` (`rfid_id`, `rfid_no`, `vehicle_id`, `created_at`) VALUES
+(67, '0010559824', NULL, '2024-09-21 23:09:13'),
+(69, '0010605475', NULL, '2024-09-21 23:10:06'),
+(70, '0004261965', NULL, '2024-09-21 23:10:09'),
+(72, '0010571220', NULL, '2024-09-21 23:10:14'),
+(85, '0004669639', NULL, '2024-09-22 00:18:09'),
+(91, '0005398438', 57, '2024-09-22 00:28:33'),
+(96, '0011238051', 56, '2024-09-22 00:35:47');
 
 -- --------------------------------------------------------
 
@@ -113,7 +133,7 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`user_id`, `emp_no`, `lastname`, `firstname`, `email`, `contactnumber`, `password`, `created_at`) VALUES
 (28, 'SUM2021-00366', 'Pelaez', 'Emanuel Luis', 'e.luis.pelaez@gmail.com', '09661970287', 'scrypt:32768:8:1$VRpws9r4l4fV2jyh$e58ec91f5368668e98430baa72099a648476fbef539a5e6ed6793eb5847b15009b5630f8b0b720af5e55e357c896a05f4740f2e91d4e141bcc8b7392b1b534f3', '2024-09-16 18:42:01'),
 (29, 'GTC2021-00232', 'Abrera', 'Juliecca', 'julieccaabrera@gmail.com', '09764829258', 'scrypt:32768:8:1$Nuz49hxARNlKbbG1$7a99f563f406b5dac5b02d6df03f53cff7a297cb732a42432a67d77ab5604f8f4a44a527fee2919e03cfc3dfe4f7317fc359a98f22dc653ba0ec6bc0f8363110', '2024-09-16 18:43:46'),
-(30, 'SUM2020-00944', 'Romero', 'Vinz Albert', 'vinzzzz@gmail.com', '09661970287', 'scrypt:32768:8:1$dTivDKdgdIXc2eTk$2337f1d9abbea4a90b305fe7a794fcf184f915b921744faa168f8e6c874c07160e8a15b41c392217e1fc3ead857cb4304c8a5f20a2d4b5268ac39b9d07b12dd0', '2024-09-16 18:45:06'),
+(30, 'SUM2020-00296', 'Romero', 'Vinz Albert', 'vinzzzz@gmail.com', '09661970287', 'scrypt:32768:8:1$dTivDKdgdIXc2eTk$2337f1d9abbea4a90b305fe7a794fcf184f915b921744faa168f8e6c874c07160e8a15b41c392217e1fc3ead857cb4304c8a5f20a2d4b5268ac39b9d07b12dd0', '2024-09-16 18:45:06'),
 (31, 'SUM2020-00284', 'Facundo', 'Ford', 'fooorrddddd@gmail.com', '09752837485', 'scrypt:32768:8:1$oYCmNxudcenM1RiY$a06b1c9f4320d668134bc97a5deb4129bb94f5546b5b2fae7c9eeade2618bdedae23f81d16de6c5249134be1f840e4f6dc46e9cb0cde54ba735ba0802481e38a', '2024-09-16 18:47:33'),
 (32, 'SLU-GB-007', 'Laygo', 'Ro-eh Israel', 'roooeeeehhhhh@gmail.com', '09752837485', 'scrypt:32768:8:1$6IjftieF04MuXSAu$c0453dc8db3d18534636f8516fc00cb9cec904cbff28f334842ba84804762656093f60bf9a6a801d2de3e32ec360a6d9333ed33002939d3988e07355612687a0', '2024-09-16 18:48:39'),
 (33, 'SLU-GB-008', 'Angelo', 'Shem', 'sheeeeemmm@gmail.com', '09661970287', 'scrypt:32768:8:1$o7cbPl6O25AoSPWj$916cca9c005597853a343944f96a88976c02d6e7ebd4233914f653dffaaf8522cf1ce084c258fc8a3d46696cf18f13fcab9237c0ea48eb6715fa5229c081cdbd', '2024-09-16 18:50:06'),
@@ -121,7 +141,7 @@ INSERT INTO `user` (`user_id`, `emp_no`, `lastname`, `firstname`, `email`, `cont
 (36, 'WUP-CC-069', 'Puyat', 'Yuri', 'yuriiiiiii@gmail.com', '09764274738', 'scrypt:32768:8:1$a1UelWpvxmq37kxY$a72f1eb203710349946d2acff7417bb5ffe964dfd49ee8ae5b0d5ec633375533a56427c48817cc2456c292ded024c0994746ee9ed31aeb6d3fccab2dddefd9d7', '2024-09-16 18:52:25'),
 (37, 'GTC2020-00690', 'Ferry', 'Ryz', 'ryyyyzzz@gmail.com', '09661972948', 'scrypt:32768:8:1$S3FXJ9dKedZsxTom$fcb28360cf32907379750dd3a53179c9544285d43ad15d2b669b2dbce945d72fa774ab674ac49a31d7b6c054e90ca08260676b40f816b95a047500159b67ef49', '2024-09-16 18:52:59'),
 (38, 'CLSU-0069', 'Grospe', 'Ellmar', 'elllmaaaarrrr@gmail.com', '09752845782', 'scrypt:32768:8:1$5Jco7kbeQjG7HOh3$766cae1bceb624e6fb1e98343fe407d84edc2be70d1afa31cbb4b671288e1402d0061e52432e01a932e9e43e40bc3aa422d375a081bf411d9257dd6fb5dd1274', '2024-09-16 18:53:55'),
-(39, 'SUM2020-006969', 'Cucio', 'Julian CARLOS', 'juliiiaaaannn@gmail.com', '09661972373', 'scrypt:32768:8:1$UuTqWUGgSjnWeoXC$0c77ba035f0ce5b4ac788efcf7621d407fed2994100374cb1e7e2ca3601716437c2844a85aff49d8fd5e5fa46132e1565dc619f8b67b66e61ce3b24862b5fdfa', '2024-09-16 18:57:09'),
+(39, 'SUM2020-002650', 'Cucio', 'Julian CARLOS', 'juliiiaaaannn@gmail.com', '09661972373', 'scrypt:32768:8:1$UuTqWUGgSjnWeoXC$0c77ba035f0ce5b4ac788efcf7621d407fed2994100374cb1e7e2ca3601716437c2844a85aff49d8fd5e5fa46132e1565dc619f8b67b66e61ce3b24862b5fdfa', '2024-09-16 18:57:09'),
 (40, 'SUM2021-000368', 'Dumbell', 'Omen ', 'scatter@gmail.com', '09764274590', 'scrypt:32768:8:1$xUyU2QTjdDqwGTF5$27f58bf3fca52b4ce673579a8b0efe8aee624f788a4fc5e79b99b9a0f9de003b98fe707adeb67b5346aa98bfc7f7e0fcadad1836467e5be78025b6da8f214953', '2024-09-16 22:46:02'),
 (41, 'SUM2020-006123', 'Punk', 'CM', 'cmpunk@gmail.com', '09758248274', 'scrypt:32768:8:1$4NKrmWzGz4uufSU7$b5de24c02b9a445471f620f6ea6c540c93ac8b6a2e509c24e0417addd6f0933bd8688c9b14a6b81fefe3ade378f2e64455b9b145d95e4a24eb658ac95ea1a66c', '2024-09-18 18:54:14');
 
@@ -140,6 +160,13 @@ CREATE TABLE `user_activity_log` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `user_activity_log`
+--
+
+INSERT INTO `user_activity_log` (`activity_id`, `user_id`, `activity_type`, `activity_timestamp`, `details`, `created_at`) VALUES
+(14, 28, 'Login Successful', '2024-09-22 00:44:47', 'Login Successful - User ID: 28', '2024-09-22 00:44:47');
+
 -- --------------------------------------------------------
 
 --
@@ -153,6 +180,14 @@ CREATE TABLE `vehicle` (
   `model` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+INSERT INTO `vehicle` (`vehicle_id`, `user_id`, `licenseplate`, `model`, `created_at`) VALUES
+(56, 32, 'ROE 123', 'Motor', '2024-09-22 00:36:02'),
+(57, 39, '666', 'Herrera Outlaw', '2024-09-22 00:39:37');
 
 --
 -- Indexes for dumped tables
@@ -226,13 +261,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `admin_activity_log`
 --
 ALTER TABLE `admin_activity_log`
-  MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `rfid`
 --
 ALTER TABLE `rfid`
-  MODIFY `rfid_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `rfid_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 
 --
 -- AUTO_INCREMENT for table `time_logs`
@@ -250,13 +285,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user_activity_log`
 --
 ALTER TABLE `user_activity_log`
-  MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `vehicle`
 --
 ALTER TABLE `vehicle`
-  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- Constraints for dumped tables
