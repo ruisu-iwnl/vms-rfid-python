6d5e4c4f6093172a053e1b37a9012c5a72738cc7
Luis Pelaez <e.luis.pelaez@gmail.com>
2024-09-22 22:06:58 +0800

    cars database

--- app/models/database.py
+++ app/models/database.py
@@ -18,10 +18,30 @@ def get_db_connection():
         print(f"Error connecting to MySQL database: {err}")
         return None
 
+def get_cars_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("CARS_DB_NAME")  
+        )
+        return connection
+    except Error as err:
+        print(f"Error connecting to 'cars' database: {err}")
+        return None
+
 def get_cursor():
     connection = get_db_connection()
     if connection is None:
-        raise Exception("Failed to connect to the database.")
+        raise Exception("Failed to connect to the main database.")
+    cursor = connection.cursor()
+    return cursor, connection
+
+def get_cars_cursor():
+    connection = get_cars_db_connection()
+    if connection is None:
+        raise Exception("Failed to connect to the 'cars' database.")
     cursor = connection.cursor()
     return cursor, connection
 
