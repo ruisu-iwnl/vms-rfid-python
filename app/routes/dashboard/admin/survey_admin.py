226684bd671f11b7b90411ca0d80c2bacd503fc0
Luis Pelaez <e.luis.pelaez@gmail.com>
2024-11-13 00:29:02 +0800

    commented out survey

--- app/routes/dashboard/admin/survey_admin.py
+++ app/routes/dashboard/admin/survey_admin.py
@@ -1,14 +1,14 @@
 
-from flask import Blueprint, render_template, session
-from app.routes.utils.session import check_access
+# from flask import Blueprint, render_template, session
+# from app.routes.utils.session import check_access
 
-survey_admin_bp = Blueprint('survey_admin', __name__)
+# survey_admin_bp = Blueprint('survey_admin', __name__)
 
-@survey_admin_bp.route('')
-def survey_admin():
-    response = check_access('admin')
+# @survey_admin_bp.route('')
+# def survey_admin():
+#     response = check_access('admin')
     
-    if response:
-        return response
+#     if response:
+#         return response
     
-    return render_template('/dashboard/admin/survey-admin.html')
+#     return render_template('/dashboard/admin/survey-admin.html')
--- app/routes/dashboard/user/survey_user.py
+++ app/routes/dashboard/user/survey_user.py
@@ -1,14 +1,14 @@
 
-from flask import Blueprint, render_template, session
-from app.routes.utils.session import check_access
+# from flask import Blueprint, render_template, session
+# from app.routes.utils.session import check_access
 
-survey_user_bp = Blueprint('survey_user', __name__)
+# survey_user_bp = Blueprint('survey_user', __name__)
 
-@survey_user_bp.route('')
-def survey_user():
-    response = check_access('user')
+# @survey_user_bp.route('')
+# def survey_user():
+#     response = check_access('user')
     
-    if response:
-        return response
+#     if response:
+#         return response
     
-    return render_template('/dashboard/user/survey-user.html')
+#     return render_template('/dashboard/user/survey-user.html')
