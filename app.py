7681e910eef5d2ae8beeb3f8823ba5258d6825fe
Luis Pelaez <e.luis.pelaez@gmail.com>
2024-11-14 00:43:07 +0800

    added path to ssl cert

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from app import create_app
 
-# It fetches create_app from __init__.py
 app = create_app()
 
 # SSL certificate paths
 context = (
-    '/etc/ssl/certs/selfsigned-cert.pem',  
-    '/etc/ssl/private/selfsigned-key.pem'
+    '/etc/ssl/certs/selfsigned-cert.pem',  # Full certificate (public)
+    '/etc/ssl/private/selfsigned-key.pem'   # Private key
 )
 
 if __name__ == '__main__':
+
     app.run(debug=True, host='0.0.0.0', port=8443, ssl_context=context)
