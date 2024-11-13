7fb03f36f6602b84ed0d2d22e4d9395f0912830c
Luis Pelaez <e.luis.pelaez@gmail.com>
2024-11-13 19:56:50 +0800

    added profile info to user side

--- app/routes/dashboard/user/user_dashboard.py
+++ app/routes/dashboard/user/user_dashboard.py
@@ -4,7 +4,7 @@ from app.routes.dashboard.user.charts.timeinoutcompare import get_time_in_time_o
 from app.routes.dashboard.user.charts.peakhours import get_peak_hours_of_vehicle_entries
 from app.routes.dashboard.user.charts.durationofstay import get_vehicle_stay_durations
 from app.routes.dashboard.user.charts.mostactivedays import most_active_days
-from app.routes.utils.utils import get_name
+from app.routes.utils.utils import get_name, get_user_profile_info
 
 user_dashboard_bp = Blueprint('user_dashboard', __name__)
 
@@ -14,16 +14,20 @@ def user_dashboard():
     if response:
         return response
 
-    user_name = get_name(session['user_id']) 
+    user_id = session['user_id']
+    user_name = get_name(user_id)
 
-    time_in_time_out_data = get_time_in_time_out_comparison(session['user_id'])
+    time_in_time_out_data = get_time_in_time_out_comparison(user_id)
     peak_hours_data = get_peak_hours_of_vehicle_entries()
-    vehicle_stay_durations_data = get_vehicle_stay_durations(session['user_id'])
-    most_active_days_data = most_active_days(session['user_id'])
+    vehicle_stay_durations_data = get_vehicle_stay_durations(user_id)
+    most_active_days_data = most_active_days(user_id)
+
+    user_profile = get_user_profile_info(user_id)
 
     return render_template('dashboard/user/user_dashboard.html',
                             user_name=user_name,
                             time_in_time_out_data=time_in_time_out_data,
                             peak_hours_data=peak_hours_data,
                             vehicle_stay_durations_data=vehicle_stay_durations_data,
-                            most_active_days_data=most_active_days_data)
+                            most_active_days_data=most_active_days_data,
+                            user=user_profile) 
