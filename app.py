24902ecdae8fd3393a502df9cdb02ad89dc5f368
Luis Pelaez <e.luis.pelaez@gmail.com>
2024-11-14 01:54:02 +0800

    updated testing field, and added certs for windows and linux

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+import os
 from app import create_app
 
 app = create_app()
 
-# SSL certificate paths
-context = (
-    '/etc/ssl/certs/selfsigned-cert.pem',  # Full certificate (public)
-    '/etc/ssl/private/selfsigned-key.pem'   # Private key
-)
+linux_cert_path = '/etc/ssl/certs/selfsigned-cert.pem'
+linux_key_path = '/etc/ssl/private/selfsigned-key.pem'
+
+windows_cert_path = r'C:\certs\selfsigned-cert.pem'
+windows_key_path = r'C:\certs\selfsigned-key.pem'
+
+if os.name == 'nt':  
+    cert_path = windows_cert_path
+    key_path = windows_key_path
+else: 
+    cert_path = linux_cert_path
+    key_path = linux_key_path
+
+context = (cert_path, key_path)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8443, ssl_context=context)
--- test_app.py
+++ test_app.py
@@ -3,13 +3,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'app', 'templates'))
 
+linux_cert_path = '/etc/ssl/certs/selfsigned-cert.pem'
+linux_key_path = '/etc/ssl/private/selfsigned-key.pem'
+
+windows_cert_path = r'C:\certs\selfsigned-cert.pem'
+windows_key_path = r'C:\certs\selfsigned-key.pem'
+
+if os.name == 'nt':  
+    cert_path = windows_cert_path
+    key_path = windows_key_path
+else:  
+    cert_path = linux_cert_path
+    key_path = linux_key_path
+
 @app.route('/test')
 def test_route():
     return render_template('test.html')
 
 if __name__ == '__main__':
-    context = (
-        '/etc/ssl/certs/selfsigned-cert.pem',
-        '/etc/ssl/private/selfsigned-key.pem'
-    )
+    context = (cert_path, key_path)
     app.run(debug=True, host='0.0.0.0', port=8443, ssl_context=context)
