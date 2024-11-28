b21ef56db04439d20f5690d960b9805ea1e3db86
Luis Pelaez <e.luis.pelaez@gmail.com>
2024-11-28 22:05:19 +0800

    removed some dashboard content

--- app/routes/dashboard/admin/charts/basic_dashboarddata.py
+++ app/routes/dashboard/admin/charts/basic_dashboarddata.py
@@ -7,42 +7,42 @@ def get_dashboard_data():
     cursor.execute('SELECT COUNT(*) FROM user')
     total_users = cursor.fetchone()[0]
     
-    # Get total number of vehicles
-    cursor.execute('SELECT COUNT(*) FROM vehicle')
-    total_vehicles = cursor.fetchone()[0]
+    # # Get total number of vehicles
+    # cursor.execute('SELECT COUNT(*) FROM vehicle')
+    # total_vehicles = cursor.fetchone()[0]
     
-    # Get average number of vehicles per user
-    cursor.execute('''
-        SELECT AVG(vehicle_count) 
-        FROM (
-            SELECT COUNT(*) AS vehicle_count
-            FROM vehicle
-            GROUP BY user_id
-        ) AS subquery
-    ''')
-    avg_vehicles_per_user = cursor.fetchone()[0] or 0
-    avg_vehicles_per_user = round(avg_vehicles_per_user, 2)
+    # # Get average number of vehicles per user
+    # cursor.execute('''
+    #     SELECT AVG(vehicle_count) 
+    #     FROM (
+    #         SELECT COUNT(*) AS vehicle_count
+    #         FROM vehicle
+    #         GROUP BY user_id
+    #     ) AS subquery
+    # ''')
+    # avg_vehicles_per_user = cursor.fetchone()[0] or 0
+    # avg_vehicles_per_user = round(avg_vehicles_per_user, 2)
     
-    # Get number of users with no vehicles
-    cursor.execute('''
-        SELECT COUNT(*) 
-        FROM user u
-        LEFT JOIN vehicle v ON u.user_id = v.user_id
-        WHERE v.vehicle_id IS NULL
-    ''')
-    users_with_no_vehicles = cursor.fetchone()[0]
+    # # Get number of users with no vehicles
+    # cursor.execute('''
+    #     SELECT COUNT(*) 
+    #     FROM user u
+    #     LEFT JOIN vehicle v ON u.user_id = v.user_id
+    #     WHERE v.vehicle_id IS NULL
+    # ''')
+    # users_with_no_vehicles = cursor.fetchone()[0]
     
-    # Get total number of RFID tags
-    cursor.execute('SELECT COUNT(*) FROM rfid')
-    total_rfid_tags = cursor.fetchone()[0]
+    # # Get total number of RFID tags
+    # cursor.execute('SELECT COUNT(*) FROM rfid')
+    # total_rfid_tags = cursor.fetchone()[0]
 
-    # Get total RFID tags registered to a vehicle
-    cursor.execute('''
-        SELECT COUNT(*)
-        FROM rfid
-        WHERE vehicle_id IS NOT NULL
-    ''')
-    registered_rfid_count = cursor.fetchone()[0]
+    # # Get total RFID tags registered to a vehicle
+    # cursor.execute('''
+    #     SELECT COUNT(*)
+    #     FROM rfid
+    #     WHERE vehicle_id IS NOT NULL
+    # ''')
+    # registered_rfid_count = cursor.fetchone()[0]
 
 
     # Get recent user registrations (last 30 days)
@@ -68,11 +68,11 @@ def get_dashboard_data():
 
     return {
         'total_users': total_users,
-        'total_vehicles': total_vehicles,
-        'avg_vehicles_per_user': avg_vehicles_per_user,
-        'users_with_no_vehicles': users_with_no_vehicles,
-        'total_rfid_tags': total_rfid_tags,
-        'registered_rfid_count': registered_rfid_count,
+        # 'total_vehicles': total_vehicles,
+        # 'avg_vehicles_per_user': avg_vehicles_per_user,
+        # 'users_with_no_vehicles': users_with_no_vehicles,
+        # 'total_rfid_tags': total_rfid_tags,
+        # 'registered_rfid_count': registered_rfid_count,
         # 'recent_user_registrations': recent_user_registrations,
         # 'most_common_vehicle_model': most_common_vehicle_model
     }
